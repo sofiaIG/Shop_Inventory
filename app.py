d5cb4cd76f9ff8731ff575ba40ddb6ed9f2c0ac1
sofiaIG <sphignatiadi@gmail.com>
2022-07-11 21:02:23 +0100

    Add order_blueprint

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template
 from controllers.plant_controller import inventory_blueprint
 from controllers.manufacturer_controller import manufacturer_blueprint
+from controllers.order_controller import order_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(inventory_blueprint)
 app.register_blueprint(manufacturer_blueprint)
+app.register_blueprint(order_blueprint)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
