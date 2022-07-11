d8765e96bc35b765f86ac200fc24b86097d67bcd
sofiaIG <sphignatiadi@gmail.com>
2022-07-11 21:01:52 +0100

    Format file

--- db/run_sql.py
+++ db/run_sql.py
@@ -1,17 +1,18 @@
-import psycopg2  
+import psycopg2
 import psycopg2.extras as ext
 
-def run_sql(sql, values = None):
+
+def run_sql(sql, values=None):
     conn = None
     results = []
-    
+
     try:
-        conn=psycopg2.connect("dbname=plant_inventory user=ignat password=201400040")
-        cur = conn.cursor(cursor_factory=ext.DictCursor)   
+        conn = psycopg2.connect("dbname=plant_inventory user=ignat password=201400040")
+        cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
         results = cur.fetchall()
-        cur.close()           
+        cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
--- repositories/plant_repository.py
+++ repositories/plant_repository.py
@@ -6,10 +6,16 @@ from models.plant import Plant
 def save(plant):
     sql = "INSERT INTO plants (name, description,  buying_cost,\
         selling_price, manufacturer_id, stock_quantity) VALUES (%s, %s, %s, %s, %s, %s) RETURNING *"
-    values = [plant.name, plant.description, plant.buying_cost,\
-        plant.selling_price, plant.manufacturer.id, plant.stock_quantity]
+    values = [
+        plant.name,
+        plant.description,
+        plant.buying_cost,
+        plant.selling_price,
+        plant.manufacturer.id,
+        plant.stock_quantity,
+    ]
     results = run_sql(sql, values)
-    id = results[0]['id']
+    id = results[0]["id"]
     plant.id = id
     return plant
 
@@ -18,10 +24,17 @@ def select_all():
     plants = []
     sql = "SELECT * FROM plants"
     result = run_sql(sql)
-    
+
     for row in result:
-        plant = Plant(row['name'], row['description'], row['buying_cost'],
-        row['selling_price'], row['manufacturer_id'], row['stock_quantity'], row['id'])
+        plant = Plant(
+            row["name"],
+            row["description"],
+            row["buying_cost"],
+            row["selling_price"],
+            row["manufacturer_id"],
+            row["stock_quantity"],
+            row["id"],
+        )
         plants.append(plant)
     return plants
 
@@ -30,30 +43,43 @@ def select(id):
     plant = None
     sql = "SELECT * FROM plants WHERE id = %s"
     values = [id]
-    result = run_sql(sql, values)[0]
-    if result is not None:
+    result = run_sql(sql, values)
+    if result is not None and len(result) > 0 and result[0] is not None:
+        result = result[0]
         manufacturer = manufacturer_repository.select(result["manufacturer_id"])
-        plant = Plant(result['name'], result['description'], result['buying_cost'], result['selling_price'],
-        manufacturer, result['stock_quantity'], result['id'])
+        plant = Plant(
+            result["name"],
+            result["description"],
+            result["buying_cost"],
+            result["selling_price"],
+            manufacturer,
+            result["stock_quantity"],
+            result["id"],
+        )
     return plant
 
 
-    
 def delete(id):
     sql = "DELETE FROM plants WHERE id = %s"
     values = [id]
     run_sql(sql, values)
-    
+
+
 def delete_all():
     sql = "DELETE FROM plants"
     run_sql(sql)
 
+
 def update(plant):
     sql = "UPDATE plants SET (name, description, buying_cost, \
         selling_price, manufacturer_id, stock_quantity) = (%s, %s, %s, %s, %s, %s) WHERE id = %s"
-    manufacturer_id = plant.manufacturer
-    manufacturer_object = manufacturer_repository.select(manufacturer_id)
-    values = [plant.name, plant.description, plant.buying_cost, plant.selling_price,\
-        manufacturer_object.id, plant.stock_quantity, plant.id]
+    values = [
+        plant.name,
+        plant.description,
+        plant.buying_cost,
+        plant.selling_price,
+        plant.manufacturer.id,
+        plant.stock_quantity,
+        plant.id,
+    ]
     run_sql(sql, values)
-
